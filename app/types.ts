2efa9bde10d3f27f783ff8453f1409dea9b97ff1
Matias-DR <Matias-DR@outlook.com>
2024-03-11 09:35:03 -0300

    ADD: project configuration

---
+++ app/types.ts
@@ -0,0 +1,16 @@
+export enum Quotation {
+  OFFICIAL = 1,
+  BLUE = 2,
+  MEP = 3,
+  CCL = 4,
+  LEDES = 5
+}
+
+export interface Dollar extends Record<string, number> {
+  buying_price: number
+  selling_price: number
+  variation_buying_price: number
+  variation_selling_price: number
+}
+
+export type Dollars = Record<Quotation, Dollar>
