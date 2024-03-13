a8ecd5d24f6249bbf4f65fef769e3b06af575518
Matias-DR <Matias-DR@outlook.com>
2024-03-13 16:14:04 -0300

    MOD: home style

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,26 @@
-import CachedDollar from '@/cached-dollar'
+import { CachedDollar } from '@/components'
+import { FRONT_BASE_URL } from '@/constants'
+import Link from 'next/link'
+
+const emojis = ['💲', '💰', '🏦', '💸', '💵']
+const link = <Link
+  key='home-link'
+  href={FRONT_BASE_URL}
+  className='text-emerald-500 hover:text-emerald-700'
+>
+  DOLARINFO
+</Link>
 
 export default async function Page() {
-  return <main
-    key='main'
-    className='p-4'
-  >
+  const emoji = emojis[Math.floor(Math.random() * emojis.length)]
+
+  return <>
     <h1
-      key='title'
-      className='mb-4 text-center font-bold'
+      key='home-title'
+      className='text-center text-xl sm:text-2xl md:text-4xl xl:text-6xl font-bold'
     >
-      DOLARIZ.AR
+      {emoji} {link} {emoji}
     </h1>
     <CachedDollar />
-  </main>
+  </>
 }
