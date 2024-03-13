7178053995c81b66252dd229e60e9d09e63a421c
Matias-DR <Matias-DR@outlook.com>
2024-03-13 16:14:40 -0300

    REF

--- components/cached-dollar.tsx
+++ components/cached-dollar.tsx
@@ -1,128 +1,79 @@
-'use client'
+import { DollarCard } from '@/components'
+import { FRONT_BASE_URL } from '@/constants'
+import { Quotation } from '@/types'
+import api from '@/app/api'
+import { cache } from 'react'
 
-import { Dollars, Quotation } from '@/types'
-
-import useSWR from 'swr'
-
-const dollarProps: Record<Quotation, Record<string, string | number>> = {
+const dollarProps: Record<Quotation, Record<string, string>> = {
   [Quotation.OFFICIAL]: {
-    key: 'official-dollar',
-    title: 'Dolar Official'
+    htmlKey: 'official-dollar',
+    title: 'DÓLAR OFICIAL',
+    link: `${FRONT_BASE_URL}historical/${Quotation.OFFICIAL}/`
   },
   [Quotation.BLUE]: {
-    key: 'blue-dollar',
-    title: 'Dolar Blue'
+    htmlKey: 'blue-dollar',
+    title: 'DÓLAR BLUE',
+    link: `${FRONT_BASE_URL}historical/${Quotation.BLUE}/`
   },
   [Quotation.CCL]: {
-    key: 'ccl-dollar',
-    title: 'Dolar CCL'
+    htmlKey: 'ccl-dollar',
+    title: 'DÓLAR CCL',
+    link: `${FRONT_BASE_URL}historical/${Quotation.CCL}/`
   },
   [Quotation.MEP]: {
-    key: 'mep-dollar',
-    title: 'Dolar MEP'
+    htmlKey: 'mep-dollar',
+    title: 'DÓLAR MEP',
+    link: `${FRONT_BASE_URL}historical/${Quotation.MEP}/`
   },
   [Quotation.LEDES]: {
-    key: 'ledes',
-    title: 'LEDES'
+    htmlKey: 'ledes',
+    title: 'LEDES',
+    link: `${FRONT_BASE_URL}historical/${Quotation.LEDES}/`
   }
 }
 
-const dollarCard = (
-  quotation: Quotation,
-  buying_price: number | string,
-  selling_price: number | string
-) => {
-  const { key, title } = dollarProps[quotation]
+export const getDollars = cache(async () => {
+  try {
+    const { data: dollars } = await api.get('dollar/multiple/')
+    return dollars
+  } catch {
+    return null
+  }
+})
 
-  return <section
-    key={`${key}-section`}
-    className='size-full'
-  >
-    <a
-      key={`${key}-link`}
-      href='#'
-      className='size-full flex gap-2 flex-col justify-center items-center bg-green-500 bg-opacity-25 rounded-lg border-4 border-amber-200 border-opacity-50 hover:bg-opacity-45'
-    >
-      <h2
-        key={`${key}-title`}
-        className='text-xl font-bold'
-      >
-        {title}
-      </h2>
-      <div
-        key={`${key}-buying-price-container`}
-        className='text-center'
-      >
-        <h3
-          key={`${key}-buying-title`}
-          className=''
-        >
-          Compra
-        </h3>
-        <p
-          key={`${key}-buying-price`}
-          className='text-4xl font-bold'
-        >
-          ${buying_price}
-        </p>
-      </div>
-      <div
-        key={`${key}-selling-price-container`}
-        className='text-center'
-      >
-        <h3
-          key={`${key}-selling-title`}
-          className=''
-        >
-          Venta
-        </h3>
-        <p
-          key={`${key}-selling-price`}
-          className='text-4xl font-bold'
-        >
-          ${selling_price}
-        </p>
-      </div>
-    </a>
-  </section>
-}
+// Revalidate every 12 hours
+export const revalidate = 3600 * 12
 
-export default function CachedDollar() {
-  const { data, error } = useSWR<Dollars>('multiple/')
+export default async function CachedDollar() {
+  const dollars = await getDollars()
 
   const dollar = (quotation: Quotation) => {
-    const loading = dollarCard(
-      quotation,
-      '...',
-      '...'
-    )
-
-    if (!data || error) return loading
-
-    const dollar = data[quotation]
-
-    if (!dollar) return loading
-
-    const prices = {
-      buying_price: String(dollar.buying_price).replace('.', ','),
-      selling_price: String(dollar.selling_price).replace('.', ',')
-    }
-
-    return dollarCard(
-      quotation,
-      prices.buying_price,
-      prices.selling_price
-    )
+    const dollar = dollars[quotation]
+    const props = dollarProps[quotation]
+    if (!dollar) return <DollarCard
+      buying_price='...'
+      selling_price='...'
+      htmlKey={props.htmlKey}
+      title={props.title}
+      link={props.link}
+    />
+    return <DollarCard
+      buying_price={dollar.buying_price}
+      selling_price={dollar.selling_price}
+      htmlKey={props.htmlKey}
+      title={props.title}
+      link={props.link}
+    />
   }
 
   return <article
     key='cached-dollar-article'
-    className='grid gap-4 grid-cols-1 grid-rows-5 sm:grid-cols-2 sm:grid-rows-3 lg:grid-cols-3 lg:grid-rows-2 xl:grid-cols-5 xl:grid-rows-1'
+    className='grid gap-4 grid-cols-2 grid-rows-3 md:grid-cols-3 md:grid-rows-2 lg:grid-cols-5 lg:grid-rows-1'
   >
     {dollar(Quotation.OFFICIAL)}
     {dollar(Quotation.BLUE)}
-    {dollar(Quotation.CCL)}
     {dollar(Quotation.MEP)}
+    {dollar(Quotation.CCL)}
     {dollar(Quotation.LEDES)}
   </article >
 }
